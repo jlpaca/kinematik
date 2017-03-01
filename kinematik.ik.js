9f94020d61550c6eacf80428a5f56bb1b8fe0f51
jhjuhn <jhjuhn.lok@gmail.com>
2017-03-01 23:19:32 +0000

    DLS with SVD (pre-SDLS commit)

--- kinematik.ik.js
+++ kinematik.ik.js
@@ -2,12 +2,14 @@ kinematik.Ik = function(subject){
 	this.subject = subject;
 }
 
-kinematik.Ik.prototype.DLS = function(target){
+// DLS:	takes the target frame as an argument and attempts to solve the
+// 	inverse kinematics problem numerically using the DLS method.
+kinematik.Ik.prototype.DLS = function(targframe){
 	var lambda = 0.05;
 	var max_iter = 12;
 	var it;
 	for (it = 0; it < max_iter; ++it) {
-		if (this.DLSstep(target, lambda)) { break; }
+		if (this.DLSstep(targframe, lambda)) { break; }
 	}
 	if (it == max_iter-1) {
 		console.log("! DLS: failed to converge");
@@ -34,13 +36,65 @@ kinematik.Ik.prototype.DLSstep = function(targframe, lambda){
 	dth = numeric.dotMV(dth, delta);
 
 	// implement the actuations.
-	
 	for (var i = 0; i < this.subject.links.length-1; ++i) {
 		var j = i + 1;
 		this.subject.links[j].increment(dth[i]);
 	}
 
 	return 0;
+}
+
+// SDLS: solve inverse kinematics with SLDS.
+kinematik.Ik.prototype.SDLS = function(targframe){
+	var it = 0; var max_iter = 12;
+	for (; it < max_iter; ++it) {
+		if (this.SDLSstep(targframe)) { break; }
+	}
+	if (it == max_iter - 1) {
+		console.log("! SDLS failed to converge");
+	}
+
+}
+kinematik.Ik.prototype.SDLSstep = function(targframe){
+	// error vector & jacobian
+	var lambda = 0.05;
+	var delta = this.delta_fromframe(targframe);
+	var J = this.jacobian();
+	var Jt = numeric.transpose(J);
+
+	var r = this.subject.links.length-1;
+
+	// perform the SVD decomposition: J = U S Vt
+	var SVD = numeric.svd(J);
+	
+	var U = SVD.U;
+	var Ut = numeric.transpose(SVD.U);
+
+	var V = SVD.V;
+	var Vt = numeric.transpose(SVD.V);
+
+	var S = SVD.S;
+
+	// sigma_1^r tau_i v_i (u_i)T
+	//var DLSmatrix = numeric.sub(numeric.identity(r), numeric.identity(r));
+	var DLSmatrix = [];
+	for (var i = 0; i < r; ++i) { DLSmatrix[i] = [];
+	for (var j = 0; j < 6; ++j) { DLSmatrix[i][j] = 0; }}
+
+	for (var i = 0; i < r; ++i) {
+		var tau_i = S[i]/(S[i]*S[i]+lambda*lambda);
+		DLSmatrix = numeric.add(DLSmatrix,
+		numeric.mul(tau_i,
+		numeric.dot(numeric.transpose([Vt[i]]), [Ut[i]])))
+	}
+
+	var dth = numeric.dotMV(DLSmatrix, delta);
+	// implement the actuations.
+	for (var i = 0; i < this.subject.links.length-1; ++i) {
+		var j = i + 1;
+		this.subject.links[j].increment(dth[i]);
+	}
+
 }
 
 // delta:	returns the difference between the target location and the
